94f865c89bd64f112cb6ad42beff0f31197219b4
sowmyaaa1 <sowmya272002@gmail.com>
2024-02-21 12:02:17 +0530

    Second commit

--- stockApp.py
+++ stockApp.py
@@ -1,4 +1,4 @@
-import streamlit as st, pandas as pd , numpy as np, yfinance as yf
+import streamlit as st, pandas as pd , numpy as np
 import plotly.express as px
 from jugaad_data.nse import stock_df
 from datetime import date
